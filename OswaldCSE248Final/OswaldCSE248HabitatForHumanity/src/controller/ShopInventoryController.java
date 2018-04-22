a88640a56fe68e3ef54b91b82c46d3e6d163b8fb
Alexis Oswald <alexisoswald98@gmail.com>
2018-04-22 14:28:29 -0400

    first page of shop is functional along with accessing database and getting all inventory.

--- OswaldCSE248Final/OswaldCSE248HabitatForHumanity/src/controller/ShopInventoryController.java
+++ OswaldCSE248Final/OswaldCSE248HabitatForHumanity/src/controller/ShopInventoryController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.Stack;
 
 import application.Main;
 import javafx.event.ActionEvent;
@@ -11,19 +12,35 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.ImageViewBuilder;
+import model.Item;
 import model.ShopInventoryModel;
 
 public class ShopInventoryController implements Initializable {
 	ShopInventoryModel sim = new ShopInventoryModel();
 	Main main = new Main();
-	
+	Stack<Item> allItemsStack;
+
+	@FXML
+	private ImageView img1;
+	@FXML
+	private ImageView img2;
+	@FXML
+	private ImageView img3;
+	@FXML
+	private ImageView img4;
+	@FXML
+	private ImageView img5;
+
 	@FXML
 	private Label welcomeLbl;
 	@FXML
 	private Button backBtn;
 	@FXML
 	private Button nextBtn;
-	
+
 	@FXML
 	private Label titleLbl1;
 	@FXML
@@ -45,7 +62,7 @@ public class ShopInventoryController implements Initializable {
 	private Label quantityLbl4;
 	@FXML
 	private Label quantityLbl5;
-	
+
 	@FXML
 	private Label priceLbl1;
 	@FXML
@@ -56,21 +73,20 @@ public class ShopInventoryController implements Initializable {
 	private Label priceLbl4;
 	@FXML
 	private Label priceLbl5;
-	
-	@FXML 
+
+	@FXML
 	private Label itemNumLbl1;
-	@FXML 
+	@FXML
 	private Label itemNumLbl2;
-	@FXML 
+	@FXML
 	private Label itemNumLbl3;
-	@FXML 
+	@FXML
 	private Label itemNumLbl4;
-	@FXML 
+	@FXML
 	private Label itemNumLbl5;
-	
-	
+
 	public void shop(ActionEvent event) throws IOException {
-		 main.changeScene("ShopView.fxml", welcomeLbl);
+		main.changeScene("ShopView.fxml", welcomeLbl);
 	}
 
 	public void myProfile(ActionEvent event) throws IOException, SQLException {
@@ -78,7 +94,7 @@ public class ShopInventoryController implements Initializable {
 	}
 
 	public void myCart(ActionEvent event) throws IOException {
-		 main.changeScene("MyCartView.fxml", welcomeLbl);
+		main.changeScene("MyCartView.fxml", welcomeLbl);
 
 	}
 
@@ -94,11 +110,75 @@ public class ShopInventoryController implements Initializable {
 		main.changeScene("LogoutView.fxml", welcomeLbl);
 	}
 
+	public void fillFields() {
+		Item currentItem;
+		currentItem = allItemsStack.pop();
+		titleLbl1.setText(currentItem.getItemName());
+		quantityLbl1.setText(Integer.toString(currentItem.getQuantity()));
+		String price1 = Double.toString(currentItem.getPrice());
+		priceLbl1.setText("$" + price1);
+		itemNumLbl1.setText(currentItem.getIdNumber());
+		Image image1 = new Image(
+				getClass().getResource("/InventoryImages/" + currentItem.getImageURL()).toExternalForm());
+		img1.setImage(image1);
+
+		currentItem = allItemsStack.pop();
+		titleLbl2.setText(currentItem.getItemName());
+		quantityLbl2.setText(Integer.toString(currentItem.getQuantity()));
+		String price2 = Double.toString(currentItem.getPrice());
+		priceLbl2.setText("$" + price2);
+		itemNumLbl2.setText(currentItem.getIdNumber());
+		Image image2 = new Image(
+				getClass().getResource("/InventoryImages/" + currentItem.getImageURL()).toExternalForm());
+		img2.setImage(image2);
+
+		currentItem = allItemsStack.pop();
+		titleLbl3.setText(currentItem.getItemName());
+		quantityLbl3.setText(Integer.toString(currentItem.getQuantity()));
+		String price3 = Double.toString(currentItem.getPrice());
+		priceLbl3.setText("$" + price3);
+		itemNumLbl3.setText(currentItem.getIdNumber());
+		Image image3 = new Image(
+				getClass().getResource("/InventoryImages/" + currentItem.getImageURL()).toExternalForm());
+		img3.setImage(image3);
+
+		currentItem = allItemsStack.pop();
+		titleLbl4.setText(currentItem.getItemName());
+		quantityLbl4.setText(Integer.toString(currentItem.getQuantity()));
+		String price4 = Double.toString(currentItem.getPrice());
+		priceLbl4.setText("$" + price4);
+		itemNumLbl4.setText(currentItem.getIdNumber());
+		Image image4 = new Image(
+				getClass().getResource("/InventoryImages/" + currentItem.getImageURL()).toExternalForm());
+		img4.setImage(image4);
+
+		currentItem = allItemsStack.pop();
+		titleLbl5.setText(currentItem.getItemName());
+		quantityLbl5.setText(Integer.toString(currentItem.getQuantity()));
+		String price5 = Double.toString(currentItem.getPrice());
+		priceLbl5.setText("$" + price5);
+		itemNumLbl5.setText(currentItem.getIdNumber());
+		Image image5 = new Image(
+				getClass().getResource("/InventoryImages/" + currentItem.getImageURL()).toExternalForm());
+		img5.setImage(image5);
+
+	}
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		sim.getInventoryFromDatabase();
+		try {
+			sim.getInventoryFromDatabase();
+			if (sim.getNumberOfItems() < 5) {
+				nextBtn.setVisible(false);
+			} else {
+				nextBtn.setVisible(true);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		allItemsStack = sim.getAllItems();
 		backBtn.setVisible(false);
-		// if number of db entries > 5, set backBtn.setVisible(true); else false
+		fillFields();
 	}
 
 }
--- OswaldCSE248Final/OswaldCSE248HabitatForHumanity/src/model/ShopInventoryModel.java
+++ OswaldCSE248Final/OswaldCSE248HabitatForHumanity/src/model/ShopInventoryModel.java
@@ -1,27 +1,71 @@
 package model;
 
 import java.sql.Connection;
-import java.util.ArrayList;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Stack;
 
 import controller.SQLiteConnection;
 
 public class ShopInventoryModel {
 	private Connection connection;
-	private ArrayList<Item> allItems = new ArrayList<Item>();
+	private Stack<Item> allItems = new Stack<Item>();
 	private int numberOfItems = 0;
 
-	public void connectToDB() {
+	public ShopInventoryModel() {
 		connection = SQLiteConnection.connect();
 		if (connection == null) {
 			System.exit(1);
 		}
 	}
 
-	public void getInventoryFromDatabase() {
-		// when each item is accessed, create a new item object and then insert
-		// it into the arraylist 'allItems' and increment number of items
+	public void getInventoryFromDatabase() throws SQLException {
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		String query = "select * from Inventory";
+		try {
+			preparedStatement = connection.prepareStatement(query);
+			resultSet = preparedStatement.executeQuery();
+			while (resultSet.next()) {
+				String idNumber = resultSet.getString("ID Number");
+				String name = resultSet.getString("Name");
+				Double price = resultSet.getDouble("Price");
+				String categories = resultSet.getString("Category");
+				String[] categoriesArray = categories.split(" ");
+				String imageURL = resultSet.getString("Image");
+				int quantity = resultSet.getInt("Quantity");
+				if (quantity != 0) {
+					Item item = new Item(idNumber, name, price, quantity, categoriesArray, imageURL);
+					allItems.push(item);
+					numberOfItems++;
+				}
 
-		// return allItems
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			preparedStatement.close();
+			resultSet.close();
+		}
 
 	}
+
+	public Stack<Item> getAllItems() {
+		return allItems;
+	}
+
+	public void setAllItems(Stack<Item> allItems) {
+		this.allItems = allItems;
+	}
+
+	public int getNumberOfItems() {
+		return numberOfItems;
+	}
+
+	public void setNumberOfItems(int numberOfItems) {
+		this.numberOfItems = numberOfItems;
+	}
+
 }
